9092fc102c837dc4c9e448651dcc8d7805e11b74
Bobby <bobbyhplau@gmail.com>
2019-06-03 23:28:59 +0000

    Function now grabs and prints all avatar_urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,32 @@
 var request = require('request');
+var auth = require('./secrets.js').GITHUB_TOKEN;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  var options = {
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': auth
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+function downloadImageByURL(url, filePath) {
+
+}
+
+getRepoContributors('jquery', 'jquery', function(err, result) {
+
+  json = JSON.parse(result);
+
+  for (i of json) {
+    console.log(i.avatar_url);
+  }
+});
