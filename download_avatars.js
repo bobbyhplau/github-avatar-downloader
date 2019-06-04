38efd9b530d98e88a4008e7d3e84b0ac7919e238
Bobby <bobbyhplau@gmail.com>
2019-06-04 02:16:43 +0000

    Fixing indents.

--- download_avatars.js
+++ download_avatars.js
@@ -8,43 +8,42 @@ var repo = process.argv[3];
 var missingErr = "You need to specify both the GitHub repo owner and the GitHub repo name.";
 
 if (!(owner && repo)) {
-  throw missingErr;
-}
+    throw missingErr;}
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  var options = {
-    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': "token " + auth
-    }
-  };
+    var options = {
+        url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': "token " + auth
+        }
+    };
 
-  request(options, function(err, res, body) {
-    cb(err, body);
-    console.log("Download Complete");
-  });
+    request(options, function(err, res, body) {
+        cb(err, body);
+        console.log("Download Complete");
+    });
 }
 
 function downloadImageByURL(url, filePath) {
 
-  request.get(url)
-         .on('error', function (err) {
+    request.get(url)
+        .on('error', function(err) {
             throw err;
-         })
-         .pipe(fs.createWriteStream(filePath));
+        })
+        .pipe(fs.createWriteStream(filePath));
 }
 
 getRepoContributors(owner, repo, function(err, result) {
 
-  var json = JSON.parse(result);
+    var json = JSON.parse(result);
 
-  for (var i of json) {
-    var fP = "./avatars/" + i.login + ".png";
-    console.log("Getting image for " , i.login);
-    downloadImageByURL(i.avatar_url, fP);
-  }
+    for (var i of json) {
+        var fP = "./avatars/" + i.login + ".png";
+        console.log("Getting image for ", i.login);
+        downloadImageByURL(i.avatar_url, fP);
+    }
 });
